bf189ead4b383d6efa7186ec8b3a9a7654918c9d
jpittmanpc <jpittmanpc@gmail.com>
2016-08-15 13:34:27 -0400

    violationlistener

---
+++ app/src/main/java/com/tidevalet/ViolationListener.java
@@ -0,0 +1,14 @@
+package com.tidevalet;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Justin on 8/15/2016.
+ */
+public interface ViolationListener {
+
+    void clicked(View view);
+
+    void violationTypes(String string);
+}
